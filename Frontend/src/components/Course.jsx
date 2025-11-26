43120164b023f7947f22022dba5885f997fcf27c
Atri Chandra <121769804+AtriChandra@users.noreply.github.com>
2025-11-26 11:53:52 +0530

    commit message

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import axios from  'axios'
+import Cards from './Cards'
+import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import { useEffect } from 'react'
+const Course = () => {
+  const [book,setBook]=useState([])
+  useEffect(()=>{
+    const getBooks=async()=>{
+      try{
+        const res=await axios.get('http://localhost:4001/books')
+        console.log(res.data)
+        setBook(res.data)
+      }catch(error){
+        console.log(error)
+      }
+    }
+    getBooks();
+  },[])
+  return (
+    <>
+    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4  dark:bg-slate-900 dark:text-white dark:border'>
+        <div className='mt-28 items-center justify-center text-center'>
+            <h1 className="text-2xl md:text-4xl">We're delighted to have you <span className='text-pink-500'> Here! :)</span></h1>
+            <p className='mt-12'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, assumenda consequuntur maxime enim aperiam praesentium. Voluptate, consectetur eum! Laudantium soluta rerum cumque magni consequatur autem tempore! Odio laudantium, voluptatibus ea animi hic nobis tempore? Sunt incidunt, repellendus nihil dignissimos dolore expedita mollitia nobis error quo at alias ullam magni voluptas et pariatur obcaecati iste eum modi voluptatem a totam. Nihil.</p>
+            <Link to='/'><button className='mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300'>Back</button></Link>
+            
+        </div>
+        <div className='mt-12 grid grid-cols-1 md:grid-cols-3'>
+          {
+            book.map((item)=>(
+              <Cards key={item.id} item={item}/>
+            ))
+          }
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Course
